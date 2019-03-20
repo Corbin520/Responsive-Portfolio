398f231018251f9508824a788cdd483602a08353
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-03-20 14:13:04 -0600

    adding javascript

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAXptncHORTkRB7_WgC6aT3m4bLYhgllDY",
+    authDomain: "portfolio-9c166.firebaseapp.com",
+    databaseURL: "https://portfolio-9c166.firebaseio.com",
+    projectId: "portfolio-9c166",
+    storageBucket: "portfolio-9c166.appspot.com",
+    messagingSenderId: "903055970588"
+  };
+  firebase.initializeApp(config);
