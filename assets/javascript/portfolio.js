32a4b6d79d14a40b26a0bb767632264d3e5d69c7
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-03-20 15:31:05 -0600

    contact info being pushed to firebase now

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -10,3 +10,22 @@
     messagingSenderId: "903055970588"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+$("#submit-button").on("click", function (event) {
+    // hide the page after click and thank for input
+    var nameText = $("#name-input").val().trim();
+    var emailText = $("#email-input").val().trim();
+    var commentText = $("#message-input").val().trim();
+  
+    database.ref().push({
+        name: nameText,
+        email: emailText,
+        comments: commentText,
+      });
+});
+
+$("#firstT").on("click", function (event) {
+    // update git hub projects on click of images
+})
