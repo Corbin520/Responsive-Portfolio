cecc7ebb7aa8b6251740eb2dccf84ce80f1585bd
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-04-04 14:13:36 -0600

    mobile responsive

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Responsive-Portfolio
+
+this is my portfolio. It is mobile responsive and is connected to my most recent projects.
--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -1,5 +1,29 @@
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAXptncHORTkRB7_WgC6aT3m4bLYhgllDY",
+    authDomain: "portfolio-9c166.firebaseapp.com",
+    databaseURL: "https://portfolio-9c166.firebaseio.com",
+    projectId: "portfolio-9c166",
+    storageBucket: "portfolio-9c166.appspot.com",
+    messagingSenderId: "903055970588"
+  };
+  firebase.initializeApp(config);
 
+  var database = firebase.database();
+
+$("#submit-button").on("click", function (event) {
+    // hide the page after click and thank for input
+    var nameText = $("#name-input").val().trim();
+    var emailText = $("#email-input").val().trim();
+    var commentText = $("#message-input").val().trim();
+  
+    database.ref().push({
+        name: nameText,
+        email: emailText,
+        comments: commentText,
+      });
+});
 
 // This was borrowed from "https://codepen.io/gschier/pen/jkivt"
 var TxtRotate = function(el, toRotate, period) {
